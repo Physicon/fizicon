9b27ac269b0f48b63dc72d8e4d34c58c8489562c
kelar86 <kelar86@mail.ru>
2019-02-26 01:25:16 +0700

    filter component styles

--- src/app/components/filters/filters.component.ts
+++ src/app/components/filters/filters.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { SubjectSubscriber } from 'rxjs/internal/Subject';
-import { getLocaleExtraDayPeriodRules } from '@angular/common';
 
 @Component({
   selector: 'app-filters',
@@ -8,32 +6,36 @@ import { getLocaleExtraDayPeriodRules } from '@angular/common';
     <div class="container">
       <form class="filters-form pure-form pure-form-stacked">
         <div class="pure-g"> 
-          <div class="pure-u-1 pure-u-md-1-3 pure-u-lg-1-4">
-            <select class="pure-input-3-4">
+          <div class="filter-form-select pure-u-1 pure-u-md-1-3 pure-u-lg-1-4">
+            <select class="filter-form__select pure-input-1">
               <option value="">Все предметы</option>
               <option *ngFor="let item of subjects" value="item">{{item}}</option>
             </select>
           </div>
-          <div class="pure-u-1 pure-u-md-1-3 pure-u-lg-1-4">
-            <select class="pure-input-3-4">
+          <div class="filter-form-select pure-u-1 pure-u-md-1-3 pure-u-lg-1-4">
+            <select class="filter-form__select pure-input-1">
               <option value="">Все жанры</option>
               <option *ngFor="let item of genres" value="item">{{item}}</option>
             </select>
           </div>
-          <div class="pure-u-1 pure-u-md-1-3 pure-u-lg-1-4">
-            <select class="pure-input-3-4">
+          <div class="filter-form-select pure-u-1 pure-u-md-1-3 pure-u-lg-1-4">
+            <select class="filter-form__select pure-input-1">
               <option value="">Все классы</option>
               <option *ngFor="let item of grades" value="item">{{item}}</option>
             </select>
           </div>
-          <div class="pure-u-1 pure-u-md-1 pure-u-lg-1-4">
-            <input class="pure-input-1" type="text" placeholder="Поиск"/>
-          </div>
+          <div class="filter-form-select filter-form-search pure-u-1 pure-u-md-1 pure-u-lg-1-4">
+            <input class="filter-form__input pure-input-1" type="text" placeholder="Поиск">
+           <!-- <button type="submit" class="pure-button"> 
+              <i class="fa fa-search fa-2x pure-img" aria-hidden="true" ></i>
+            </button> --> 
+         </div>
+        
         </div>  
       </form>
     </div>
   `,
-  styles: []
+  styleUrls: ['./filters.component.css']
 })
 export class FiltersComponent implements OnInit {
 
